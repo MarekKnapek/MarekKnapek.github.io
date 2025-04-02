afddce75cec5c10250b1ab18ae65a9b4cdc42567
Marek Knápek <knapek.mar@gmail.com>
2025-04-02 12:55:07 +0200

    mk_clib_hasher.wasm

--- hash/mkch.js
+++ hash/mkch.js
@@ -1,9 +1,93 @@
 "use strict";
 
-let g_mkch_wi;
-let g_file_state;
+let mkch_g_wi;
+let mkch_g_file_state;
 
-function mkch_onfinished(computed)
+function mkch_is_integer_non_negative(x)
+{
+	let gud = true;
+	gud = gud && typeof x === "number";
+	gud = gud && isFinite(x);
+	gud = gud && Math.round(x) == x;
+	gud = gud && x >= 0;
+	gud = gud && x <= 2 * 1024 * 1024;
+	return gud;
+}
+
+function mkch_is_integer_positive(x)
+{
+	let gud = true;
+	gud = gud && typeof x === "number";
+	gud = gud && isFinite(x);
+	gud = gud && Math.round(x) == x;
+	gud = gud && x >= 1;
+	gud = gud && x <= 2 * 1024 * 1024;
+	return gud;
+}
+
+function mkch_sub_block(block, off, len)
+{
+	console.assert(block instanceof Uint8Array);
+	console.assert(mkch_is_integer_non_negative(off));
+	console.assert(mkch_is_integer_positive(len));
+	console.assert(block.byteLength > off);
+	console.assert(block.byteLength >= off + len);
+
+	const sub = new Uint8Array(block.buffer, block.byteOffset + off, len);
+	return sub;
+}
+
+function mkch_memcpy_impl(dst_buff, dst_off, src_buff, src_off, amount)
+{
+	console.assert(dst_buff instanceof Uint8Array);
+	console.assert(mkch_is_integer_non_negative(dst_off));
+	console.assert(src_buff instanceof Uint8Array);
+	console.assert(mkch_is_integer_non_negative(src_off));
+	console.assert(mkch_is_integer_positive(amount));
+	console.assert(dst_buff.byteLength > dst_off);
+	console.assert(dst_buff.byteLength >= dst_off + amount);
+	console.assert(src_buff.byteLength > src_off);
+	console.assert(src_buff.byteLength >= src_off + amount);
+
+	const dst_tmp = mkch_sub_block(dst_buff, dst_off, amount);
+	const src_tmp = mkch_sub_block(src_buff, src_off, amount);
+	dst_tmp.set(src_tmp, 0);
+}
+
+function mkch_block_from_memory(memory)
+{
+	console.assert(memory instanceof WebAssembly.Memory);
+
+	const block = new Uint8Array(memory.buffer, 0, memory.buffer.byteLength);
+	return block;
+}
+
+function mkch_memcpy(dst_buff, dst_off, src_buff, src_off, amount)
+{
+	console.assert(dst_buff instanceof Uint8Array || dst_buff instanceof WebAssembly.Memory);
+	console.assert(mkch_is_integer_non_negative(dst_off));
+	console.assert(src_buff instanceof Uint8Array || src_buff instanceof WebAssembly.Memory);
+	console.assert(mkch_is_integer_non_negative(src_off));
+	console.assert(mkch_is_integer_positive(amount));
+	console.assert(!(dst_buff instanceof WebAssembly.Memory && src_buff instanceof WebAssembly.Memory));
+
+	if(dst_buff instanceof WebAssembly.Memory)
+	{
+		const dst_tmp = mkch_block_from_memory(dst_buff);
+		mkch_memcpy_impl(dst_tmp, dst_off, src_buff, src_off, amount);
+	}
+	else if(src_buff instanceof WebAssembly.Memory)
+	{
+		const src_tmp = mkch_block_from_memory(src_buff);
+		mkch_memcpy_impl(dst_buff, dst_off, src_tmp, src_off, amount);
+	}
+	else
+	{
+		mkch_memcpy_impl(dst_buff, dst_off, src_buff, src_off, amount);
+	}
+}
+
+function mkch_on_finished(computed)
 {
 	if(computed == false)
 	{
@@ -41,32 +125,39 @@ function mkch_on_chunk_read(args, file_state, hash_state, file_pos_new, file_siz
 	}
 	const chunk_data = event.target.result;
 	const chunk_size = chunk_data.byteLength;
-	new Uint8Array(g_mkch_wi.exports.memory.buffer, hash_state.m_msg_addr, hash_state.m_msg_size).set(new Uint8Array(chunk_data, 0, chunk_size), 0);
-	const appended = g_mkch_wi.exports.mkch_append(chunk_size);
-	if(appended == 0)
+	const mem = mkch_sub_block(mkch_block_from_memory(mkch_g_wi.exports.memory), hash_state.m_buffer_buf, hash_state.m_buffer_len);
+	const dat = mkch_sub_block(new Uint8Array(chunk_data), 0, chunk_size);
+	mkch_memcpy(mem, 0, dat, 0, chunk_size);
+	const appended = mkch_g_wi.exports.mk_clib_hasher_append(chunk_size);
+	if(appended != 0)
 	{
-		mkch_onfinished(false);
+		mkch_on_finished(false);
 		file_state.m_on_done();
 		return;
 	}
 	if(file_pos_new != file_size)
 	{
 		const str = ((file_pos_new * 100.0) / file_size).toFixed(2) + " %";
-		mkch_onfinished(str);
+		mkch_on_finished(str);
 		read_again();
 	}
 	else
 	{
-		const finished = g_mkch_wi.exports.mkch_finish(args.m_xof_len);
-		if(!(finished != 0 && (finished >= 1 && finished <= hash_state.m_digest_size)))
+		const finished = mkch_g_wi.exports.mk_clib_hasher_finish(args.m_xof_len);
+		if(finished != 0)
 		{
-			mkch_onfinished(false);
+			mkch_on_finished(false);
 			file_state.m_on_done();
 			return;
 		}
-		const digest_buff = new Uint8Array(g_mkch_wi.exports.memory.buffer, hash_state.m_digest_addr, finished);
-		const digest_str = mkch_buff_to_str(digest_buff);
-		mkch_onfinished(digest_str);
+		const digest_len = mkch_g_wi.exports.mk_clib_hasher_get_digest_len(args.m_alg_idx);
+		if(!(digest_len == 0 || (digest_len >= 16 && digest_len <= 64)))
+		{
+			return false;
+		}
+		const digest_buf = mkch_sub_block(mkch_block_from_memory(mkch_g_wi.exports.memory), hash_state.m_buffer_buf, digest_len == 0 ? args.m_xof_len : digest_len);
+		const digest_str = mkch_buff_to_str(digest_buf);
+		mkch_on_finished(digest_str);
 		file_state.m_on_done();
 		return;
 	}
@@ -75,7 +166,7 @@ function mkch_on_chunk_read(args, file_state, hash_state, file_pos_new, file_siz
 function mkch_read_chunk(args, file_state, hash_state, file, file_pos_cur, file_size, self)
 {
 	const file_rem = file_size - file_pos_cur;
-	const chunk_size = Math.min(file_rem, hash_state.m_msg_size);
+	const chunk_size = Math.min(file_rem, hash_state.m_buffer_len);
 	const file_pos_new = file_pos_cur + chunk_size;
 	const file_slice = file.slice(file_pos_cur, file_pos_new);
 	const read_again = function(){ self(args, file_state, hash_state, file, file_pos_new, file_size, self); };
@@ -88,30 +179,13 @@ function mkch_hash_refresh_impl_file(args, file_state)
 {
 	const hash_state =
 	{
-		m_alg_name_addr: g_mkch_wi.exports.mkch_get_alg_name_addr(),
-		m_alg_name_size: g_mkch_wi.exports.mkch_get_alg_name_size(),
-		m_msg_addr: g_mkch_wi.exports.mkch_get_msg_addr(),
-		m_msg_size: g_mkch_wi.exports.mkch_get_msg_size(),
-		m_digest_addr: g_mkch_wi.exports.mkch_get_digest_addr(),
-		m_digest_size: g_mkch_wi.exports.mkch_get_digest_size(),
+		m_buffer_buf: mkch_g_wi.exports.mk_clib_hasher_get_buffer_buf(),
+		m_buffer_len: mkch_g_wi.exports.mk_clib_hasher_get_buffer_len(),
 	};
-	if(!(args.m_alg_name_len >= 1 && args.m_alg_name_len <= hash_state.m_alg_name_size))
+	const inited = mkch_g_wi.exports.mk_clib_hasher_init(args.m_alg_idx);
+	if(inited != 0)
 	{
-		mkch_onfinished(false);
-		file_state.m_on_done();
-		return;
-	}
-	if(!(args.m_xof_len >= 1 && args.m_xof_len <= hash_state.m_digest_size))
-	{
-		mkch_onfinished(false);
-		file_state.m_on_done();
-		return;
-	}
-	new Uint8Array(g_mkch_wi.exports.memory.buffer, hash_state.m_alg_name_addr, hash_state.m_alg_name_size).set(new Uint8Array(args.m_alg_name.buffer, 0, args.m_alg_name_len), 0);
-	const inited = g_mkch_wi.exports.mkch_init(args.m_alg_name_len);
-	if(inited == 0)
-	{
-		mkch_onfinished(false);
+		mkch_on_finished(false);
 		file_state.m_on_done();
 		return;
 	}
@@ -121,39 +195,47 @@ function mkch_hash_refresh_impl_file(args, file_state)
 	mkch_read_chunk(args, file_state, hash_state, file, file_pos_cur, file_size, mkch_read_chunk);
 }
 
-function mkch_hash_compute(alg_name, alg_name_len, xof_len, data, data_len)
+function mkch_hash_compute(alg_idx, xof_len, data, data_len)
 {
-	const alg_name_addr = g_mkch_wi.exports.mkch_get_alg_name_addr();
-	const alg_name_size = g_mkch_wi.exports.mkch_get_alg_name_size();
-	const msg_addr = g_mkch_wi.exports.mkch_get_msg_addr();
-	const msg_size = g_mkch_wi.exports.mkch_get_msg_size();
-	const digest_addr = g_mkch_wi.exports.mkch_get_digest_addr();
-	const digest_size = g_mkch_wi.exports.mkch_get_digest_size();
-	if(!(alg_name_len >= 1 && alg_name_len <= alg_name_size))
+	const buffer_buf = mkch_g_wi.exports.mk_clib_hasher_get_buffer_buf();
+	const buffer_len = mkch_g_wi.exports.mk_clib_hasher_get_buffer_len();
+	if(!(data_len >= 0 && data_len <= buffer_len))
 	{
 		return false;
 	}
-	if(!(data_len >= 0 && data_len <= msg_size))
+	if(!(xof_len >= 1 && xof_len <= buffer_len))
 	{
 		return false;
 	}
-	if(!(xof_len >= 1 && xof_len <= digest_size))
+	const digest_len = mkch_g_wi.exports.mk_clib_hasher_get_digest_len(alg_idx);
+	if(!(digest_len == 0 || (digest_len >= 16 && digest_len <= 64)))
 	{
 		return false;
 	}
-	new Uint8Array(g_mkch_wi.exports.memory.buffer, alg_name_addr, alg_name_size).set(new Uint8Array(alg_name.buffer, 0, alg_name_len), 0);
-	new Uint8Array(g_mkch_wi.exports.memory.buffer, msg_addr, msg_size).set(new Uint8Array(data.buffer, 0, data_len), 0);
-	const mkch_ret = g_mkch_wi.exports.mkch(alg_name_len, data_len, xof_len);
-	if(mkch_ret == 0)
+	const inited = mkch_g_wi.exports.mk_clib_hasher_init(alg_idx);
+	if(inited != 0)
 	{
 		return false;
 	}
-	if(!(mkch_ret >= 1 && mkch_ret <= digest_size))
+	if(data_len != 0)
+	{
+		const mem = mkch_sub_block(mkch_block_from_memory(mkch_g_wi.exports.memory), buffer_buf, buffer_len);
+		const txt = mkch_sub_block(data, 0, data_len);
+		mkch_memcpy(mem, 0, txt, 0, data_len);
+	}
+	const appended = mkch_g_wi.exports.mk_clib_hasher_append(data_len);
+	if(appended != 0)
 	{
 		return false;
 	}
-	const digest_buff = new Uint8Array(g_mkch_wi.exports.memory.buffer, digest_addr, mkch_ret);
-	return mkch_buff_to_str(digest_buff);
+	const finished = mkch_g_wi.exports.mk_clib_hasher_finish(xof_len);
+	if(finished != 0)
+	{
+		return false;
+	}
+	const digest_buf = mkch_sub_block(mkch_block_from_memory(mkch_g_wi.exports.memory), buffer_buf, digest_len == 0 ? xof_len : digest_len);
+	const digest_str = mkch_buff_to_str(digest_buf);
+	return digest_str;
 }
 
 function mkch_hash_refresh_impl_text(args, file_state)
@@ -161,8 +243,8 @@ function mkch_hash_refresh_impl_text(args, file_state)
 	const text = document.getElementById("text").value;
 	const data = new Uint8Array(text.length * 3);
 	const data_len = new TextEncoder().encodeInto(text, data).written;
-	const computed = mkch_hash_compute(args.m_alg_name, args.m_alg_name_len, args.m_xof_len, data, data_len);
-	mkch_onfinished(computed);
+	const computed = mkch_hash_compute(args.m_alg_idx, args.m_xof_len, data, data_len);
+	mkch_on_finished(computed);
 	file_state.m_on_done();
 }
 
@@ -195,7 +277,7 @@ function mkch_on_done(file_state)
 	}
 	else
 	{
-		g_file_state = null;
+		mkch_g_file_state = null;
 	}
 }
 
@@ -204,9 +286,10 @@ function mkch_prepare_args()
 	const filee = document.getElementById("file");
 	const is_file = filee.value != "";
 	const alge = document.getElementById("alg");
-	const alg_name = new Uint8Array(0xff);
-	const alg_name_len = new TextEncoder().encodeInto(alge.value, alg_name).written;
-	const is_xof = alge.value == "shake_128" || alge.value == "shake_256" || alge.value == "blake3xof";
+	const alg_idx = parseInt(alge.value, 10);
+	console.assert(mkch_is_integer_non_negative(alg_idx));
+	const digest_len = mkch_g_wi.exports.mk_clib_hasher_get_digest_len(alg_idx);
+	const is_xof = digest_len == 0;
 	const xof_len_base = 64;
 	let xof_len_;
 	xof_len_ = document.getElementById("length").value;
@@ -221,8 +304,7 @@ function mkch_prepare_args()
 	{
 		m_filee: filee,
 		m_is_file: is_file,
-		m_alg_name: alg_name,
-		m_alg_name_len: alg_name_len,
+		m_alg_idx: alg_idx,
 		m_is_xof: is_xof,
 		m_xof_len: xof_len,
 		m_alg_str: alg_str,
@@ -241,41 +323,41 @@ function mkch_hash_refresh()
 	};
 	const on_done = function(){ mkch_on_done(file_state_new); };
 	file_state_new.m_on_done = on_done;
-	if(!g_file_state)
+	if(!mkch_g_file_state)
 	{
-		g_file_state = file_state_new;
+		mkch_g_file_state = file_state_new;
 		mkch_hash_refresh_impl_both(args, file_state_new);
 	}
 	else
 	{
-		g_file_state.m_next = function(){ mkch_hash_refresh_impl_both(args, file_state_new); };
-		g_file_state.m_cancel = true;
-		g_file_state = file_state_new;
+		mkch_g_file_state.m_next = function(){ mkch_hash_refresh_impl_both(args, file_state_new); };
+		mkch_g_file_state.m_cancel = true;
+		mkch_g_file_state = file_state_new;
 	}
 }
 
-function mkch_length_onchange()
+function mkch_length_on_change()
 {
 	mkch_hash_refresh();
 }
 
-function mkch_alg_onchange()
+function mkch_alg_on_change()
 {
 	mkch_hash_refresh();
 }
 
-function mkch_file_onchange()
+function mkch_file_on_change()
 {
 	mkch_hash_refresh();
 }
 
-function mkch_text_oninput()
+function mkch_text_on_input()
 {
 	document.getElementById("file").value = null;
 	mkch_hash_refresh();
 }
 
-function mkch_form_onsubmit()
+function mkch_form_on_submit()
 {
 	mkch_hash_refresh();
 	return false;
@@ -302,7 +384,7 @@ function mkch_parse_url()
 			if(o.label.toLowerCase() == h || o.value.toLowerCase() == h)
 			{
 				o.selected = true;
-				mkch_alg_onchange();
+				mkch_alg_on_change();
 				break;
 			}
 		}
@@ -318,7 +400,7 @@ function mkch_parse_url()
 			const text = document.getElementById("text");
 			const t = decodeURIComponent(f.substring(pos + 3));
 			text.value = t;
-			mkch_text_oninput();
+			mkch_text_on_input();
 		}
 	}
 }
@@ -330,54 +412,34 @@ function mkch_populate_events()
 	const file = document.getElementById("file");
 	const alge = document.getElementById("alg");
 	const length = document.getElementById("length");
-	form.addEventListener("submit", mkch_form_onsubmit);
-	text.addEventListener("input", mkch_text_oninput);
-	file.addEventListener("change", mkch_file_onchange);
-	alge.addEventListener("change", mkch_alg_onchange);
-	length.addEventListener("change", mkch_length_onchange);
+	form.addEventListener("submit", mkch_form_on_submit);
+	text.addEventListener("input", mkch_text_on_input);
+	file.addEventListener("change", mkch_file_on_change);
+	alge.addEventListener("change", mkch_alg_on_change);
+	length.addEventListener("change", mkch_length_on_change);
 }
 
 function mkch_populate_algs()
 {
-	let names = [];
-	const alg_name_addr = g_mkch_wi.exports.mkch_get_alg_name_addr();
-	const alg_name_size = g_mkch_wi.exports.mkch_get_alg_name_size();
-	const alg_count = g_mkch_wi.exports.mkch_get_alg_count();
-	if(alg_count == 0)
-	{
-		return;
-	}
-	for(let i = 0; i != alg_count; ++i)
-	{
-		const alg_name_len = g_mkch_wi.exports.mkch_get_alg_name(i);
-		if(alg_name_len == 0)
-		{
-			return;
-		}
-		const alg_name = new TextDecoder().decode(new Uint8Array(g_mkch_wi.exports.memory.buffer, alg_name_addr, alg_name_len));
-		const alg_pretty_name_len = g_mkch_wi.exports.mkch_get_alg_pretty_name(i);
-		if(alg_pretty_name_len == 0)
-		{
-			return;
-		}
-		const alg_pretty_name = new TextDecoder().decode(new Uint8Array(g_mkch_wi.exports.memory.buffer, alg_name_addr, alg_pretty_name_len));
-		names[i * 2 + 0] = alg_name;
-		names[i * 2 + 1] = alg_pretty_name;
-	}
 	const alge = document.getElementById("alg");
+	const alg_count = mkch_g_wi.exports.mk_clib_hasher_get_alg_count();
 	for(let i = 0; i != alg_count; ++i)
 	{
-		const alg_name = names[i * 2 + 0];
-		const alg_pretty_name = names[i * 2 + 1];
-		const algi = new Option(alg_pretty_name, alg_name);
+		const str_buf = mkch_g_wi.exports.mk_clib_hasher_get_alg_str_buf(i);
+		const str_len = mkch_g_wi.exports.mk_clib_hasher_get_alg_str_len(i);
+		const str_arr = new Uint8Array(mkch_g_wi.exports.memory.buffer, str_buf, str_len);
+		const str_obj = new TextDecoder().decode(str_arr);
+		const algi = new Option(str_obj, i.toString());
 		alge.add(algi, undefined);
 	}
+	const default_alg = mkch_g_wi.exports.mk_clib_hasher_get_default_alg();
+	alge.selectedIndex = default_alg;
 }
 
 function mkch_on_wasm_loaded(mkch_wm)
 {
 	const mkch_wi = mkch_wm.instance;
-	g_mkch_wi = mkch_wi;
+	mkch_g_wi = mkch_wi;
 	mkch_populate_algs();
 	mkch_populate_events();
 	mkch_parse_url();
@@ -385,7 +447,7 @@ function mkch_on_wasm_loaded(mkch_wm)
 
 function mkch_load_wasm()
 {
-	const mkch_fp = fetch("mkch.wasm");
+	const mkch_fp = fetch("mk_clib_hasher.wasm");
 	const mkch_wp = WebAssembly.instantiateStreaming(mkch_fp);
 	mkch_wp.then(mkch_on_wasm_loaded);
 }
